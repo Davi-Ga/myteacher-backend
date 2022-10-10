fe6bc8610100399149db43969bc045842f71f28e
Davi-Ga <davigaldinoky@gmail.com>
2022-10-10 08:38:33 -0300

    ajsutes

--- myteacher/settings.py
+++ myteacher/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teacher.apps.TeacherConfig',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
