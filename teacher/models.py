b14516c1bdfb2c43b56fc6f33cf6994dc363122f
Davi-Ga <davigaldinoky@gmail.com>
2022-10-11 16:59:09 -0300

    models de aula criado

--- teacher/models.py
+++ teacher/models.py
@@ -5,3 +5,8 @@ class Professor (models.Model):
     valor_hora = models.DecimalField(max_digits=9, decimal_places=2, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.URLField(max_length=225, null=False, blank=False)
+
+class Aula(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=250, null=False, blank=False)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
