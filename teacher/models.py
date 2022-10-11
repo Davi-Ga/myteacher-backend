ea5336aa8460c8a2d8389299fcd55cf63bd63c31
Davi-Ga <davigaldinoky@gmail.com>
2022-10-11 17:01:38 -0300

    migrate das aulas feita

--- teacher/models.py
+++ teacher/models.py
@@ -9,4 +9,4 @@ class Professor (models.Model):
 class Aula(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, null=False, blank=False, related_name='aulas')
