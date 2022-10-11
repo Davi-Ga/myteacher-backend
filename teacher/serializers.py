7d58f60106b3384f5ecc79dd8d54597bb617f17e
Davi-Ga <davigaldinoky@gmail.com>
2022-10-11 17:23:15 -0300

    serializer de aula e cadastroaula feito

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -1,8 +1,18 @@
 from dataclasses import field
 from rest_framework import serializers
-from teacher.models import Professor
+from teacher.models import Professor, Aula
 
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = '__all__'
+        
+class CadastrarAulaSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=250)
+    nome = serializers.CharField(max_length=100)
+    
+class AulaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aula
+        fields = '__all__'
+ 
