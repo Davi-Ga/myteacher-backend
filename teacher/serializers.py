5eb140e0aee742aa9a1a9efcdd0f46976cf72c74
Davi-Ga <davigaldinoky@gmail.com>
2022-10-11 16:59:24 -0300

    serializer do professor feita

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -1,1 +1,8 @@
+from dataclasses import field
 from rest_framework import serializers
+from teacher.models import Professor
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
